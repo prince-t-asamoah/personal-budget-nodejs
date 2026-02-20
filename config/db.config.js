3ad90c736716f8e99a21caa1e16901d3367ba120
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-20 16:37:55 +0000

    feat: update config to include ssl settings

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,13 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
 const db = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
+  ...(isProduction && { ssl: { rejectUnauthorized: false } })
 });
 
 db.on("connect", () => {
