a642bf66cda0869cd291608e9bf7bac467f12f4d
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-20 18:06:35 +0000

    feat: create an auth router

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const envelopesRouter = require('./envelopes.routes');
+const authRouter = require('./auth.routes');
 
 const indexRouter = express.Router();
 
 indexRouter.use('/envelopes', envelopesRouter);
+indexRouter.use('/auth', authRouter)
 
 
 module.exports  = indexRouter;
