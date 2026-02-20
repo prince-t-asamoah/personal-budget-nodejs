41ece4cf6c716aec0f86ff6f7a61a534750b6bd6
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-20 16:07:22 +0000

    feat: format query with comma

--- database/migrations/001_create_users.sql
+++ database/migrations/001_create_users.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS users (
     full_name VARCHAR(120) NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
-    created_at TIMESTAMPTZ DEFAULT NOW()
+    created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
