a6b971ed33ead1385a263c68e935032b5dfc977f
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-22 17:50:56 +0000

    feat: update login and signup controller to use custom error and success response dto

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,10 +1,13 @@
 const db = require("../config/db.config");
 const { SignupDto, LoginDto } = require("../dtos/auth.dtos");
+const { SuccessResponseDto } = require("../dtos/response.dtos");
 const { UserDto } = require("../dtos/users.dto");
+const { LoginError, SignupError } = require("../errors/AuthError");
 const { hashPassword, verifyPassword } = require("../util/password.util");
 
 /**
  * @typedef {import('../types/controller.types').Controller}  Controller
+ * @typedef {import('pg').QueryResult} DatabaseQuery
  */
 
 /**
@@ -14,32 +17,45 @@ const { hashPassword, verifyPassword } = require("../util/password.util");
  */
 const signup = async (req, res, next) => {
   const userData = new SignupDto(req.body);
+  let hashedPassword = "";
+  /**@type {DatabaseQuery} */
+  let queryResult;
 
   try {
-    const hashedPassword = await hashPassword(userData.password);
+    // User password hashing
+    try {
+      hashedPassword = await hashPassword(userData.password);
+    } catch (error) {
+      throw new SignupError("Password hashing failed.", 500, { cause: error });
+    }
 
-    const result = await db.query(
-      `INSERT INTO users (full_name, email, password_hash)
-       VALUES($1, $2, $3)
-       RETURNING id, full_name, email, created_at, updated_at`,
-      [userData.fullname, userData.email, hashedPassword],
-    );
-
+    // Save new user fullname, email and hashed password into database
+    try {
+      queryResult = await db.query(
+        `INSERT INTO users (full_name, email, password_hash)
+         VALUES($1, $2, $3)
+         RETURNING id, full_name, email, created_at, updated_at`,
+        [userData.fullname, userData.email, hashedPassword],
+      );
+    } catch (error) {
+      throw new SignupError(
+        "Saving new user details to database failed.",
+        500,
+        { cause: error },
+      );
+    }
     /** @type {UserDto} */
     const newData = result.rows[0];
 
-    res.json({
-      success: true,
-      message: "User signup successful",
-      data: new UserDto(newData),
-    });
+    res.json(
+      new SuccessResponseDto({
+        message: "User signup successful",
+        data: new UserDto(newData),
+      }),
+    );
   } catch (error) {
-    console.error(error);
-    res
-      .status(500)
-      .json({ success: false, message: "Internal server error", error: error });
+    next(error);
   }
-  next();
 };
 
 /**
@@ -49,65 +65,68 @@ const signup = async (req, res, next) => {
  */
 const login = async (req, res, next) => {
   if (req.session.user) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Already logged in", data: null });
+    throw new LoginError("Already logged in", 400);
   }
+
   const loginData = new LoginDto(req.body);
+  /** @type {DatabaseQuery} */
+  let findUserByEmail;
 
   try {
-    const findUser = await db.query("SELECT * FROM users WHERE email = $1", [
-      loginData.email,
-    ]);
-
-    if (findUser.rows.length === 0) {
-      return res
-        .status(404)
-        .json({ success: false, message: "User does not exist." });
+    // Search database for user by email
+    try {
+      findUserByEmail = await db.query("SELECT * FROM users WHERE email = $1", [
+        loginData.email,
+      ]);
+    } catch (error) {
+      throw new LoginError(
+        `Retrieving user with email: ${loginData.email} from database failed.`,
+      );
     }
 
-    const userData = new UserDto(findUser.rows[0]);
+    if (findUserByEmail.rows.length === 0) {
+      throw new LoginError("User does not exist.", 404);
+    }
 
-    const isPasswordValid = await verifyPassword(
-      loginData.password,
-      userData.passwordHash,
-    );
+    const userData = new UserDto(findUserByEmail.rows[0]);
+
+    let isPasswordValid = false;
+
+    try {
+      isPasswordValid = await verifyPassword(
+        loginData.password,
+        userData.passwordHash,
+      );
+    } catch (error) {
+      throw new LoginError("Hashed password verification failed.");
+    }
 
     if (!isPasswordValid) {
-      return res.status(401).json({
-        success: false,
-        message: "User email or password is not valid.",
-      });
+      throw new LoginError("User email or password is not valid.", 401);
     }
 
     if (!req.session.user) {
       req.session.user = userData;
     }
 
-    res.status(200).json({
-      success: true,
-      message: "Login successful",
-      data: {
-        id: userData.id,
-        fullName: userData.fullName,
-        email: userData.email,
-        phoneNumber: userData.phoneNumber,
-        addressDetails: userData.addressDetails,
-        profileImageUrl: userData.profileImageUrl,
-        createdAt: userData.createdAt,
-        updatedAt: userData.updatedAt,
-      },
-    });
+    res.status(200).json(
+      new SuccessResponseDto({
+        message: "Login successful",
+        data: {
+          id: userData.id,
+          fullName: userData.fullName,
+          email: userData.email,
+          phoneNumber: userData.phoneNumber,
+          addressDetails: userData.addressDetails,
+          profileImageUrl: userData.profileImageUrl,
+          createdAt: userData.createdAt,
+          updatedAt: userData.updatedAt,
+        },
+      }),
+    );
   } catch (error) {
-    console.error(error);
-    res.status(500).json({
-      success: false,
-      message: "Internal server error",
-      error: error,
-      data: null,
-    });
+    next(error);
   }
-  next();
 };
 
 module.exports = {
