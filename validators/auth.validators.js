868cc5d9fc217395dc8b798e911675db172927b9
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-22 19:04:57 +0000

    feat: update validation errors to check for undefined values

--- validators/auth.validators.js
+++ validators/auth.validators.js
@@ -2,36 +2,46 @@ const { body } = require("express-validator");
 
 const signupValidator = [
   body("fullname")
-    .notEmpty()
+    .exists({ values: "undefined" })
     .withMessage("Fullname is required")
+    .notEmpty()
+    .withMessage("Fullname cannot be empty")
     .isString()
     .withMessage("Fullname must be a string"),
   body("email")
+    .exists({ values: "undefined"})
+    .withMessage("Enail is required")
     .notEmpty()
-    .withMessage("Email is required")
+    .withMessage("Email cannot be exmpty")
     .isEmail()
     .withMessage("Valid email is required"),
   body("password")
-    .notEmpty()
+    .exists({ values: "undefined" })
     .withMessage("Password is required")
+    .notEmpty()
+    .withMessage("Password cannot be empty")
     .isLength({ min: 12 })
     .withMessage("Password must be at least 12 characters long."),
 ];
 
 const loginValidator = [
   body("email")
-    .notEmpty()
+    .exists({ values: "undefined" })
     .withMessage("Email is required")
+    .notEmpty()
+    .withMessage("Email cannot be empty")
     .isEmail()
     .withMessage("Valid email is required"),
   body("password")
-    .notEmpty()
+    .exists({ values: "undefined" })
     .withMessage("Password is required")
+    .notEmpty()
+    .withMessage("Password cannot be empty")
     .isLength({ min: 12 })
     .withMessage("Password must be at least 12 characters long."),
 ];
 
 module.exports = {
   signupValidator,
-  loginValidator
+  loginValidator,
 };
