1150f4ee31b5995531364624f8dc432cc2517053
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-28 10:42:21 +0000

    feat: create envelopes type

---
+++ types/envelope.types.js
@@ -0,0 +1,18 @@
+/**
+ * Represents a budget envelope object.
+ *
+ * @typedef {object} Envelope
+ * @property {string} id - A numbered id
+ * @property {string} name - Name of envelope e.g. "Rent", "Groceries"
+ * @property {number} allocatedAmount - Total budgeted amount
+ * @property {number} spentAmount - Amount already spent
+ * @property {number} balance - Allocated amount - Spent amount
+ * @property {string} currency - ISO code e.g. "GHS", "USD"
+ * @property {string} createdAt - ISO timestamp
+ * @property {string} updatedAt - ISO timestamp
+ *
+ */
+
+module.exports = {
+    Envelope
+}
