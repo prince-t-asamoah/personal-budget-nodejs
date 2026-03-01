076ceaeda3ff50fd78754be028f7bac7918d2e07
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-01 19:53:09 +0000

    featP update getAllEnvelopes to use envelopes dto

--- controllers/envelopes.controllers.js
+++ controllers/envelopes.controllers.js
@@ -1,11 +1,12 @@
 const db = require("../config/db.config");
 const { SuccessResponseDto } = require("../dtos/response.dtos");
 const EnvelopeError = require("../errors/EnvelopeError");
+const { EnvelopeDto } = require("../dtos/envelope.dtos");
 
 /**
  * @typedef {import('../types/controller.types').Controller}  Controller
  * @typedef {import('../types/envelope.types').Envelope} Envelope
- * @typedef {import('pg').QueryResult} DatabaseQuery
+ * @typedef {import('pg').QueryResult<Envelope>} EnvelopeQuery
  */
 
 /**
@@ -17,7 +18,7 @@ const getAllEnvelopes = async (req, res, next) => {
   const userId = req.session.user.id;
 
   try {
-    /** @type {DatabaseQuery} */
+    /** @type {EnvelopeQuery} */
     const queryResult = await db.query(
       "SELECT * FROM envelopes WHERE user_id = $1",
       [userId],
@@ -30,7 +31,7 @@ const getAllEnvelopes = async (req, res, next) => {
 
     return res.status(200).json(
       new SuccessResponseDto({
-        data,
+        data: new EnvelopeDto(data),
         message: "Fetching user envelopes successful",
       }),
     );
