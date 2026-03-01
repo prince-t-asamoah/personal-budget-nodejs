84dac734bc39bb1c1b399ef20c7b8e48606d5c6c
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-01 21:36:23 +0000

    feat: update envelope dto constructur paramaters

--- dtos/envelope.dtos.js
+++ dtos/envelope.dtos.js
@@ -4,30 +4,31 @@ class EnvelopeDto extends BaseDto {
   constructor({
     id,
     name,
-    allocatedAmount,
-    spentAmount,
+    allocated_amount,
+    spent_amount,
     balance,
     currency,
-    createdAt,
-    updatedAt,
+    created_at,
+    updated_at,
   }) {
+    super();
     /**@type {string} */
     this.id = id;
     /**@type {string} */
     this.name = name;
     /**@type {number} */
-    this.allocatedAmount = allocatedAmount;
+    this.allocatedAmount = allocated_amount;
     /**@type {number} */
-    this.spentAmount = spentAmount;
+    this.spentAmount = spent_amount;
     /**@type {number} */
     this.balance = balance;
     /**@type {number} */
     this.currency = currency;
     /**@type {string} */
-    this.createdAt = createdAt;
+    this.createdAt = created_at;
     /**@type {string} */
-    this.updatedAt = updatedAt;
-    filterUndefined();
+    this.updatedAt = updated_at;
+    this.filterUndefined();
   }
 }
 
