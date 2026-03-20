fd6393049cbb066356d704452ef0b7a7646fba90
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-20 10:25:35 +0000

    refactor: rename postgres env

--- config/db.config.js
+++ config/db.config.js
@@ -14,11 +14,11 @@ types.setTypeParser(NUMERIC_OID, val => Number(val));
 const isProduction = process.env.NODE_ENV === "production";
 
 const db = new Pool({
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  database: process.env.POSTGRES_DB_NAME,
+  user: process.env.POSTGRES_DB_USER,
+  password: process.env.POSTGRES_DB_PASSWORD,
+  host: process.env.POSTGRES_DB_HOST,
+  port: process.env.POSTGRES_DB_PORT,
   ...(isProduction && { ssl: { rejectUnauthorized: false } })
 });
 
