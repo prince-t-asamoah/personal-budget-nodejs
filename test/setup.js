10f017c183c1ea3b6ef5dfc93570186685405dde
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-04-07 11:28:32 +0000

    test: create integrationn test setup

---
+++ test/setup.js
@@ -0,0 +1,33 @@
+require("../config/loadEnv")();
+
+const db = require("../config/db.config");
+const startMigrations = require("../database/migrate");
+
+async function truncateAllTables() {
+  await db.query(`
+        DO $$ DECLARE
+            r RECORD;
+        BEGIN
+            FOR r IN (
+                SELECT tablename FROM pg_tables
+                WHERE schemaname = 'public'
+            ) LOOP
+             EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' RESTART IDENTITY CASCADE';
+            END LOOP;
+        END $$;
+        `);
+}
+
+before(async function () {
+  await startMigrations();
+  console.log("✔ Test DB ready");
+});
+
+beforeEach(async function () {
+  await truncateAllTables();
+});
+
+after(async function () {
+  await db.end();
+  console.log("✔ DB pool closed");
+});
